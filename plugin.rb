b2516d3a7052b5421831733a33b17b4852b1efe3
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-06-21 09:21:59 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# naem: below-sitemap
+# about: plugin to insert content into menu
+# version: 0.1
+# author: me
+
+register_asset "javascripts/discourse/templates/connectors/site-map-links/site-map-links.hbs"
